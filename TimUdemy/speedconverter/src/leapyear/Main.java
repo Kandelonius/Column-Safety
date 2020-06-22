6c7fa47594fa9f860e427a0f2583fb08595c92f2
Shane Kaestner <shane.kaestner@yahoo.com>
2020-06-22 12:12:48 -0700

    update

---
+++ TimUdemy/speedconverter/src/leapyear/Main.java
@@ -0,0 +1,32 @@
+package leapyear;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println(isLeapYear(-1));
+		System.out.println(isLeapYear(1600));
+		System.out.println(isLeapYear(2017));
+		System.out.println(isLeapYear(2000));
+	}
+	public static boolean isLeapYear(int year)
+	{
+		if(year < 1 || year > 9999)
+		{
+			return false;
+		}
+		if(year%4 == 0)
+		{
+			if(year%100 == 0)
+			{
+				if(year%400 == 0)
+				{
+					return true;
+				}
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+}
