d4dc5bf98c5b807d2523dc06bd0eb340210f34b6
Shane Kaestner <shane.kaestner@yahoo.com>
2020-06-22 15:00:34 -0700

    update

---
+++ TimUdemy/speedconverter/src/decimalcomparator/Main.java
@@ -0,0 +1,27 @@
+package decimalcomparator;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("decimalcomparator");
+		System.out.println(areEqualByThreeDecimalPlaces(3.1756, 3.175));
+		System.out.println(areEqualByThreeDecimalPlaces(3.176, 3.175));
+		System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+		System.out.println(areEqualByThreeDecimalPlaces(-3.175, 3.175));
+	}
+	public static boolean areEqualByThreeDecimalPlaces(double numOne, double numTwo)
+	// if(((String) numOne.length()).equals((String) numTwo.length()))
+	// {
+
+	// }
+	// (String) numOne.findIndexOf('.');
+	// double modNumOne = numOne%1, modNumTwo = numTwo%1;
+	// if((int) numOne == (int) numTwo)
+	// {
+	// 	for(int i = 0; i < )
+	// }
+	{
+		return (int) (numOne * 1000) == (int) (numTwo * 1000);
+	}
+}
