c837da1892b4a55a0ed41ece24bffb90fcdfc43c
Shane Kaestner <shane.kaestner@yahoo.com>
2020-06-23 19:56:13 -0700

    completed the leapyear method with months

--- TimUdemy/speedconverter/src/leapyear/Main.java
+++ TimUdemy/speedconverter/src/leapyear/Main.java
@@ -8,6 +8,11 @@ public class Main
 		System.out.println(isLeapYear(1600));
 		System.out.println(isLeapYear(2017));
 		System.out.println(isLeapYear(2000));
+		System.out.println(isLeapYear(8244));
+		System.out.println(getDaysInMonth(2, 2016));
+		System.out.println(getDaysInMonth(2, 1482));
+		System.out.println(getDaysInMonth(2, 6830));
+		System.out.println(getDaysInMonth(2, 8244));
 	}
 	public static boolean isLeapYear(int year)
 	{
@@ -29,4 +34,54 @@ public class Main
 		}
 		return false;
 	}
+	public static int getDaysInMonth(int month, int year)
+    {
+    	if(year < 1 || year > 9999 || month > 12 || month < 1)
+    	{
+    		return -1;
+    	}
+        switch (month) {
+            case 1:
+                return 31;
+                // break;
+            case 2:
+                
+                if (isLeapYear(year)) {
+                    return 29;
+                } else {
+                    return 28;
+                }
+                // break;
+            case 3:
+                return 31;
+                // break;
+            case 4:
+                return 30;
+                // break;
+            case 5:
+                return 31;
+                // break;
+            case 6:
+                return 30;
+                // break;
+            case 7:
+                return 31;
+                // break;
+            case 8:
+                return 31;
+                // break;
+            case 9:
+                return 30;
+                // break;
+            case 10:
+                return 31;
+                // break;
+            case 11:
+                return 30;
+                // break;
+            case 12:
+                return 31;
+        }
+        return -1;
+    }
 }
