916a4ed97e06cb72c03a69601aad695a7c584ff1
Shane Kaestner <shane.kaestner@yahoo.com>
2020-06-24 22:16:39 -0700

    created a method that checks to see if the last digit is the same for three given numbers.

--- TimUdemy/speedconverter/src/numberpalindrome/Main.java
+++ TimUdemy/speedconverter/src/numberpalindrome/Main.java
@@ -45,4 +45,16 @@ public class Main
 		}
 		return numOne % 10 == numTwo % 10 || numOne / 10 == numTwo % 10 || numOne % 10 == numTwo / 10 || numOne / 10 == numTwo / 10;
 	}
+	public static boolean hasSameLastDigit(int numOne,int numTwo, int numThree)
+	{
+		if(isValid(numOne) && isValid(numTwo) && isValid(numThree))
+		{
+			return numOne % 10 == numTwo % 10 || numOne % 10 == numThree % 10 || numTwo % 10 == numThree % 10;
+		}
+		return false;
+	}
+	public static boolean isValid(int num)
+	{
+		return num > 9 && num <= 1000;
+	}
 }
