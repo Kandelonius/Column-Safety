256994512d7f09b1d3c319a62c2d78029d1e6692
Shane Kaestner <shane.kaestner@yahoo.com>
2020-06-24 21:48:10 -0700

    created a method that will tell if a pair of two digit numbers has any digits in common.

--- TimUdemy/speedconverter/src/numberpalindrome/Main.java
+++ TimUdemy/speedconverter/src/numberpalindrome/Main.java
@@ -37,4 +37,12 @@ public class Main
 		}
 		return true;
 	}
+	public static boolean hasSharedDigit(int numOne, int numTwo)
+	{
+		if(numOne<10 || numOne>99 || numTwo<10 || numTwo>99)
+		{
+			return false;
+		}
+		return numOne % 10 == numTwo % 10 || numOne / 10 == numTwo % 10 || numOne % 10 == numTwo / 10 || numOne / 10 == numTwo / 10;
+	}
 }
