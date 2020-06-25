967665dfc48d71eb037eb253738df03b5f4610f7
Shane Kaestner <shane.kaestner@yahoo.com>
2020-06-24 17:45:50 -0700

    added a number based palindrome checker.

---
+++ TimUdemy/speedconverter/src/numberpalindrome/Main.java
@@ -0,0 +1,40 @@
+package numberpalindrome;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println(isPalindrome(121));
+		System.out.println(isPalindrome(1221));
+		System.out.println(isPalindrome(12321));
+		System.out.println(isPalindrome(11));
+		System.out.println(isPalindrome(12231));
+		System.out.println(isPalindrome(10));
+		System.out.println(isPalindrome(101));
+	}
+	public static boolean isPalindrome(int number)
+	{
+		int rebmun = 0, tempNum = number;
+		while(tempNum >= 10)
+		{
+			rebmun += tempNum%10;
+			rebmun *= 10;
+			tempNum /= 10;
+		}
+		System.out.println("outside rebmun "+rebmun+" tempNum "+tempNum);
+		rebmun += tempNum;
+		while(number > 0)
+		{
+			if(number%10 == rebmun%10)
+			{
+				number/=10;
+				rebmun/=10;
+				continue;
+			}else
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
