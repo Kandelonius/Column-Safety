0fa323cb7a4a5198acda7d5abdd78aa8cba8ddda
Shane Kaestner <shane.kaestner@yahoo.com>
2020-07-03 10:19:20 -0700

    started working on a vehicle challenge.

---
+++ TimUdemy/speedconverter/src/inheritancevehicle/AudiTTS.java
@@ -0,0 +1,14 @@
+package inheritancevehicle;
+
+public class AudiTTS extends Car
+{
+    public AudiTTS(
+        String name,
+        int doors,
+        int engineCapacity)
+    {
+        super(name,
+            doors,
+            engineCapacity);
+    }
+}
---
+++ TimUdemy/speedconverter/src/inheritancevehicle/Vehicle.java
@@ -0,0 +1,11 @@
+package inheritancevehicle;
+
+public class Vehicle
+{
+    private String name;
+
+    public Vehicle(String name)
+    {
+        this.name = name;
+    }
+}
