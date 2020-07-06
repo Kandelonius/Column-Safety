ae86673e6fa4faa14b06f660dcee0c73d3af34de
Shane Kaestner <shane.kaestner@yahoo.com>
2020-07-05 23:54:34 -0700

    wip vehicle challenge.

--- TimUdemy/speedconverter/src/inheritancevehicle/Car.java
+++ TimUdemy/speedconverter/src/inheritancevehicle/Car.java
@@ -6,11 +6,43 @@ public class Car extends Vehicle
 {
     private int doors;
     private int engineCapacity;
+    private int wheels;
+    private int gears;
+    private boolean isManual;
 
-    public Car(String name, int doors, int engineCapacity)
+    private int currentGear;
+
+    public Car(
+        String name,
+        int doors,
+        int engineCapacity,
+        int wheels,
+        int gears,
+        boolean isManual)
     {
         super(name);
         this.doors = doors;
         this.engineCapacity = engineCapacity;
+        this.wheels = wheels;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.currentGear = 1;
+    }
+
+    public void changeGear(int currentGear)
+    {
+        this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(): Changed to "+this.currentGear+" gear");
+    }
+
+    public void changeVelocity(int speed, int direction)
+    {
+        System.out.println("Car.changeVelocity(): "+speed+" direction "+direction);
+    }
+
+    @Override
+    public void stop()
+    {
+        super.stop();
     }
 }
