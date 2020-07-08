66ebb935210e6c18148d18bc8df9e3daf124fc5c
Shane Kaestner <shane.kaestner@yahoo.com>
2020-07-08 15:36:34 -0700

    followed along with the instructor solution to the challenge

--- TimUdemy/speedconverter/src/polyCar/Main.java
+++ TimUdemy/speedconverter/src/polyCar/Main.java
@@ -14,6 +14,25 @@ public class Main
                 +car.accelerate((int) (Math.random() * 9) + 1)+
                 "\nNow we should see how it handles braking : "+car.brake()+"\n");
         }
+        TimCar car = new TimCar(8, "Base Car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Ford ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Holden holden = new Holden(6, "Ford Falcon");
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
     }
 
     public static Car RandomCar()
@@ -32,5 +51,63 @@ public class Main
                 return new Generic();
         }
         return null;
+    }
+
+    static class Holden extends TimCar
+    {
+        public Holden(
+            int cylinders,
+            String name)
+        {
+            super(cylinders,
+                name);
+        }
+
+        @Override
+        public String startEngine()
+        {
+            return getClass().getSimpleName()+" -> startEngine(fancy)"; // another way of getting the class name
+        }
+
+        @Override
+        public String accelerate()
+        {
+            return "Holden -> accelerate()";
+        }
+
+        @Override
+        public String brake()
+        {
+            return "Holden -> brake()";
+        }
+    }
+
+    static class Ford extends TimCar
+    {
+        public Ford(
+            int cylinders,
+            String name)
+        {
+            super(cylinders,
+                name);
+        }
+
+        @Override
+        public String startEngine()
+        {
+            return "Ford -> startEngine()";
+        }
+
+        @Override
+        public String accelerate()
+        {
+            return "Ford -> accelerate()";
+        }
+
+        @Override
+        public String brake()
+        {
+            return "Ford -> brake()";
+        }
     }
 }
---
+++ TimUdemy/speedconverter/src/polyCar/TimCar.java
@@ -0,0 +1,73 @@
+package polyCar;
+
+public class TimCar
+{
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public TimCar(
+        int cylinders,
+        String name)
+    {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    public int getCylinders()
+    {
+        return cylinders;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String startEngine()
+    {
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate()
+    {
+        return "Car -> accelerate()";
+    }
+
+    public String brake()
+    {
+        return "Car -> brake()";
+    }
+}
+
+class Mitsubishi extends TimCar
+{
+    public Mitsubishi(
+        int cylinders,
+        String name)
+    {
+        super(cylinders,
+            name);
+    }
+
+    @Override
+    public String startEngine()
+    {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate()
+    {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake()
+    {
+        return "Mitsubishi -> brake()";
+    }
+}
