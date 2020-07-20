d1630a123b68b144c565dd1b81e5747b8da1fcaa
Shane Kaestner <shane.kaestner@yahoo.com>
2020-07-19 17:05:23 -0700

    update

--- TimUdemy/speedconverter/src/billsburgers/Burger.java
+++ TimUdemy/speedconverter/src/billsburgers/Burger.java
@@ -3,28 +3,35 @@ package billsburgers;
 public class Burger
 {
     private String name;
-    private String bun;
-//    private boolean meatless;
+
     private String meat;
+
     private double price;
-    
-    private String additional1;
+
+    private String bun;
+    //    private boolean meatless;
+
+    private String additional1Name;
+
     private double addPrice1;
 
-    private String additional2;
+    private String additional2Name;
+
     private double addPrice2;
 
-    private String additional3;
+    private String additional3Name;
+
     private double addPrice3;
 
-    private String additional4;
+    private String additional4Name;
+
     private double addPrice4;
 
     public Burger(
         String name,
-        String bun,
         String meat,
-        double price)
+        double price,
+        String bun)
     {
         this.name = name;
         this.bun = bun;
@@ -32,34 +39,64 @@ public class Burger
         this.price = price;
     }
 
-    private void addAdditionalItem1(String name, double price)
+    public void addAdditionalItem1(
+        String name,
+        double price)
     {
-        this.additional1 = name;
+        this.additional1Name = name;
         this.addPrice1 = price;
     }
 
-    private void addAdditionalItem2(String name, double price)
+    public void addAdditionalItem2(
+        String name,
+        double price)
     {
-        this.additional2 = name;
+        this.additional2Name = name;
         this.addPrice2 = price;
     }
 
-    private void addAdditionalItem3(String name, double price)
+    public void addAdditionalItem3(
+        String name,
+        double price)
     {
-        this.additional3 = name;
+        this.additional3Name = name;
         this.addPrice3 = price;
     }
 
-    private void addAdditionalItem4(String name, double price)
+    public void addAdditionalItem4(
+        String name,
+        double price)
     {
-        this.additional4 = name;
+        this.additional4Name = name;
         this.addPrice4 = price;
     }
 
     public double totalPrice()
     {
         double hambergerPrice = this.price;
-        System.out.println(this.name+" hamburger "+" on a "+this.bun+" bun"
-        +" price is "+this.price);
+        System.out.println(this.name + " hamburger " + "on a " + this.bun + " bun"
+            + " price is " + this.price);
+        if (this.additional1Name != null)
+        {
+            hambergerPrice += this.addPrice1;
+            System.out.println("Added " + this.additional1Name + " for an extra " + this.addPrice1);
+        }
+        if (this.additional2Name != null)
+        {
+            hambergerPrice += this.addPrice2;
+            System.out.println("Added " + this.additional2Name + " for an extra " + this.addPrice2);
+        }
+        if (this.additional3Name != null)
+        {
+            hambergerPrice += this.addPrice3;
+            System.out.println("Added " + this.additional3Name + " for an extra " + this.addPrice3);
+        }
+        if (this.additional4Name != null)
+        {
+            hambergerPrice += this.addPrice4;
+            System.out.println("Added " + this.additional4Name + " for an extra " + this.addPrice4);
+        }
+        System.out.println("burger price is "+hambergerPrice);
+        return hambergerPrice;
     }
 }
--- TimUdemy/speedconverter/src/billsburgers/Main.java
+++ TimUdemy/speedconverter/src/billsburgers/Main.java
@@ -4,6 +4,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Burger burger = new Burger("wheat", false, "beef");
+//      Burger(String name, String meat, double price, String bun)
+        Burger burger = new Burger("basic", "beef", 12.00, "sourdough");
+        double price = burger.totalPrice();
+        burger.addAdditionalItem1("bacon", 1.00);
+        burger.addAdditionalItem2("avocado", 0.75);
+        burger.addAdditionalItem3("cheese", 0.50);
+        price = burger.totalPrice();
+
     }
 }
