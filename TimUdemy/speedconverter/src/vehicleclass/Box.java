2421bb58a521e576ede9e10cb26fd751f56e7173
Shane Kaestner <shane.kaestner@yahoo.com>
2020-08-12 23:32:22 -0700

    update

---
+++ TimUdemy/speedconverter/src/vehicleclass/Box.java
@@ -0,0 +1,21 @@
+package vehicleclass;
+
+public class Box
+{
+    double length;
+    double height;
+    double width;
+    Box innerBox;
+
+//    public Box(
+//        double length,
+//        double height,
+//        double width,
+//        Box innerBox)
+//    {
+//        this.length = length;
+//        this.height = height;
+//        this.width = width;
+//        this.innerBox = innerBox;
+//    }
+}
