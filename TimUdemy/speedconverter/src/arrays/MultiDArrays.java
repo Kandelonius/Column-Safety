92928400ca0d39dacc34d07e7531be41702c54a3
Shane Kaestner <shane.kaestner@yahoo.com>
2020-08-22 01:07:45 -0700

    did a coding exercise.

---
+++ TimUdemy/speedconverter/src/arrays/MultiDArrays.java
@@ -0,0 +1,20 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class MultiDArrays {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int n = stdIn.nextInt();
+        // if n < 100
+        int[][] expandingNumbers = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                expandingNumbers[i][j] = Math.abs(i * j);
+                System.out.print(expandingNumbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+        stdIn.close();
+    }
+}
