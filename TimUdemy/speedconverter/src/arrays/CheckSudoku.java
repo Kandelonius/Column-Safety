8f7764c09e29d32ddf8271ce27b04f774fac2ba6
Shane Kaestner <shane.kaestner@yahoo.com>
2020-08-25 14:50:58 -0700

    completed the sudoku exercise.

--- TimUdemy/speedconverter/src/arrays/CheckSudoku.java
+++ TimUdemy/speedconverter/src/arrays/CheckSudoku.java
@@ -28,22 +28,70 @@ Sample Input 1:
 Sample Output 1:
 
 YES
+
+2
+1 2 3 4
+4 3 2 1
+2 1 4 3
+3 4 1 2
+YES
  */
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CheckSudoku {
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
         // get the dimensions of the sudoku board.
-        int N = stdIn.nextInt();
-        N = N * N;
+        int n = stdIn.nextInt();
+        int N = n * n;
         stdIn.nextLine();
+        // create the matrix using n^2 as the parameter
         int[][] matrix = new int[N][N];
         for (int i = 0; i < matrix.length; ++i) {
             for (int j = 0; j < matrix[i].length; ++j) {
                 matrix[i][j] = stdIn.nextInt();
             }
         }
+        /*
+        perhaps do something along the lines of check each line using a nested for loop
+        swapping i and j in the matrix to check each column and taking sqrt of n to find
+        the individual squares within the overall square. check 1 - n with no duplicates.
+         */
+        for (int i = 0; i < N; i++) {
+            if (N == 1 && matrix[0][0] == 1) { // check for size 1 matrix with int 1
+                System.out.println("YES");
+                break;
+            } else if (N == 1) {
+                System.out.printf("NO");
+                break;
+            }
+            int[] row = new int[N];
+            int[] column = matrix[i].clone();
+            int[] square = new int[N];
+            for (int j = 0; j < N; j++) {
+                row[j] = matrix[j][i];
+                square[j] = matrix[(i / n) * n + j / n][i * n % N + j % n];
+            }
+            if (check(column) && check(row) && check(square)) {
+                System.out.println("YES");
+                break;
+            } else {
+                System.out.println("NO");
+                break;
+            }
+        }
+    }
+
+    private static boolean check(int[] vector) {
+        int i = 0;
+        Arrays.sort(vector); // sort vector 0 - N
+        for (int num : vector) {
+            if (num != ++i) {
+                return false;
+            }
+        }
+        return true;
     }
 }
