106d840b6c0bb74d7e8ab27ef38419ee602935c0
Shane Kaestner <shane.kaestner@yahoo.com>
2020-08-30 07:43:49 -0700

    added a short constructor example using overloaded constructors.

--- TimUdemy/speedconverter/src/constructors/Account.java
+++ TimUdemy/speedconverter/src/constructors/Account.java
@@ -1,20 +1,87 @@
 package constructors;
 
 class Account {
-    public long balance;
+    public int number;
+    public double balance;
+    public String customerName;
+    public String email;
+    public String phoneNumber;
 
-    public String ownerName;
-
-    public boolean locked;
-
+    // general constructor
     public Account(
-        long balance,
-        String ownerName,
-        boolean locked) {
+        int number,
+        double balance,
+        String customerName,
+        String email,
+        String phoneNumber) {
+        this.number = number;
         this.balance = balance;
-        this.ownerName = ownerName;
-        this.locked = locked;
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
     }
 
+    // paranoid off the grid man constructor with no email of phone number.
+    public Account(
+        int number,
+        double balance,
+        String customerName) {
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+    }
 
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void addFunds(double amount) {
+        balance += amount;
+        System.out.println("Added funds, new balance is " + balance);
+    }
+
+    public void withdrawFunds(double amount) {
+        if (balance - amount < 0) {
+            System.out.println("Not enough funds!!");
+        } else {
+            balance -= amount;
+            System.out.println("Successfully withdrew " + amount + " new balance is " + balance);
+        }
+    }
 }
--- TimUdemy/speedconverter/src/constructors/Main.java
+++ TimUdemy/speedconverter/src/constructors/Main.java
@@ -2,9 +2,16 @@ package constructors;
 
 public class Main {
     public static void main(String[] args) {
+        /*
         System.out.println(ConstantsAndUtilities.A_CONSTANT_TTT);
         System.out.println(ConstantsAndUtilities.B_CONSTANT_QQQ);
         System.out.println(ConstantsAndUtilities.getMagicString());
         System.out.println(ConstantsAndUtilities.convertStringToAnotherString("aa"));
+         */
+        Account user1 = new Account(123, 1337.69, "Guy", "Guy@gmail.com", "555-555-5555");
+        Account user2 = new Account(333, 77.53, "secret");
+
+        user2.addFunds(100.33);
+        user1.withdrawFunds(10_000);
     }
 }
