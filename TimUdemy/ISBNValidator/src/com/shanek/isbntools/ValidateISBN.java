22c0f9abd26dbe2932462bfe9f074cacaa66c01a
Shane Kaestner <shane.kaestner@yahoo.com>
2020-09-26 01:49:57 -0700

    update.

--- TimUdemy/ISBNValidator/src/com/shanek/isbntools/ValidateISBN.java
+++ TimUdemy/ISBNValidator/src/com/shanek/isbntools/ValidateISBN.java
@@ -9,18 +9,25 @@ public class ValidateISBN {
         if (isbn.length() != 10 && isbn.length() != 13) {
             throw new NumberFormatException("ISBNs must be 10 digits long at this point.");
         }
-        if (isbn.length() == 13) {
-            return true;
-        }
         int total = 0;
-        for (int i = 0; i < 10; i++) {
-            if (Character.isDigit(isbn.charAt(i))) { // checking the digit is numeric
-                total += Character.getNumericValue(isbn.charAt(i)) * (10 - i); // taking a tally
-            } else { // letter x is ok for the final number
-                if (Character.toLowerCase(isbn.charAt(i)) == 'x' && i == 9) {
-                    total += 10;
-                } else {
-                    throw new NumberFormatException("ISBN not numeric.");
+        if (isbn.length() == 13) {
+            for (int i = 0; i < 12; i++) {
+                if(Character.isDigit(isbn.charAt(i)) && i % 2 == 1) {
+                    total += Character.getNumericValue(isbn.charAt(i));
+                } else if(Character.isDigit(isbn.charAt(i)) && i % 2 == 0) {
+                    total += Character.getNumericValue(isbn.charAt(i)) * 3;
+                }
+            }
+        } else {
+            for (int i = 0; i < 10; i++) {
+                if (Character.isDigit(isbn.charAt(i))) { // checking the digit is numeric
+                    total += Character.getNumericValue(isbn.charAt(i)) * (10 - i); // taking a tally
+                } else { // letter x is ok for the final number
+                    if (Character.toLowerCase(isbn.charAt(i)) == 'x' && i == 9) {
+                        total += 10;
+                    } else {
+                        throw new NumberFormatException("ISBN not numeric.");
+                    }
                 }
             }
         }
