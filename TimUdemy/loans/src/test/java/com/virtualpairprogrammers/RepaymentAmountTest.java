29909ee8adaafa012476b24c7725dd82f6ce24e2
Shane Kaestner <shane.kaestner@yahoo.com>
2020-10-04 22:14:13 -0700

    update to repaymentAmount tests.

--- TimUdemy/loans/src/test/java/com/virtualpairprogrammers/RepaymentAmountTest.java
+++ TimUdemy/loans/src/test/java/com/virtualpairprogrammers/RepaymentAmountTest.java
@@ -1,12 +1,30 @@
 package com.virtualpairprogrammers;
 
 import org.junit.Test;
-import org.mockito.Mockito.*;
+import static org.mockito.Mockito.*;
+import org.mockito.Spy;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class RepaymentAmountTest {
+
+    @Spy
+    LoanApplication loanApplication;
+
     @Test
-    public void test() {
-        fail();
+    public void testOneYearLoanWholeNumber() {
+        loanApplication = spy (new LoanApplication());
+        loanApplication.setPrincipal(1200);
+        loanApplication.setTermInMonths(12);
+        doReturn(new BigDecimal(10)).when(loanApplication).getInterestRate();
+        // TODO set interest rate to 10%
+
+        LoanCalculatorController controller = new LoanCalculatorController();
+        controller.processNewLoanApplication(loanApplication);
+
+        assertEquals(new BigDecimal(110), loanApplication.getRepayment());
     }
 }
