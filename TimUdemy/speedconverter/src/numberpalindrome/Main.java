2bd668138840c4557b94c37dca9f9db46aa8b75f
Shane Kaestner <shane.kaestner@yahoo.com>
2020-10-06 20:15:34 -0700

    added a hashtable example.

--- TimUdemy/speedconverter/src/numberpalindrome/Main.java
+++ TimUdemy/speedconverter/src/numberpalindrome/Main.java
@@ -1,9 +1,7 @@
 package numberpalindrome;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         System.out.println(isPalindrome(121));
         System.out.println(isPalindrome(1221));
         System.out.println(isPalindrome(12321));
@@ -13,26 +11,21 @@ public class Main
         System.out.println(isPalindrome(101));
     }
 
-    public static boolean isPalindrome(int number)
-    {
+    public static boolean isPalindrome(int number) {
         int rebmun = 0, tempNum = number;
-        while (tempNum >= 10)
-        {
+        while (tempNum >= 10) {
             rebmun += tempNum % 10;
             rebmun *= 10;
             tempNum /= 10;
         }
         System.out.println("outside rebmun " + rebmun + " tempNum " + tempNum);
         rebmun += tempNum;
-        while (number > 0)
-        {
-            if (number % 10 == rebmun % 10)
-            {
+        while (number > 0) {
+            if (number % 10 == rebmun % 10) {
                 number /= 10;
                 rebmun /= 10;
                 continue;
-            } else
-            {
+            } else {
                 return false;
             }
         }
@@ -41,10 +34,8 @@ public class Main
 
     public static boolean hasSharedDigit(
         int numOne,
-        int numTwo)
-    {
-        if (numOne < 10 || numOne > 99 || numTwo < 10 || numTwo > 99)
-        {
+        int numTwo) {
+        if (numOne < 10 || numOne > 99 || numTwo < 10 || numTwo > 99) {
             return false;
         }
         return numOne % 10 == numTwo % 10 || numOne / 10 == numTwo % 10 || numOne % 10 == numTwo / 10 || numOne / 10 == numTwo / 10;
@@ -53,17 +44,14 @@ public class Main
     public static boolean hasSameLastDigit(
         int numOne,
         int numTwo,
-        int numThree)
-    {
-        if (isValid(numOne) && isValid(numTwo) && isValid(numThree))
-        {
+        int numThree) {
+        if (isValid(numOne) && isValid(numTwo) && isValid(numThree)) {
             return numOne % 10 == numTwo % 10 || numOne % 10 == numThree % 10 || numTwo % 10 == numThree % 10;
         }
         return false;
     }
 
-    public static boolean isValid(int num)
-    {
+    public static boolean isValid(int num) {
         return num > 9 && num <= 1000;
     }
 }
