f9f02ecafbc00393bac939e25f45396e6a8e9c97
Shane Kaestner <shane.kaestner@yahoo.com>
2020-10-09 18:23:48 -0700

    implemented selection sort using java.

--- TimUdemy/speedconverter/src/algorithms/BubbleSort.java
+++ TimUdemy/speedconverter/src/algorithms/BubbleSort.java
@@ -24,10 +24,9 @@ public class BubbleSort {
 
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
-        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0;
-             lastUnsortedIndex--) {
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
             for (int i = 0; i < lastUnsortedIndex; i++) {
-                if (intArray[i] > intArray[i + 1]) {
+                if (intArray[i] > intArray[i + 1]) { // this is why this implementation is stable
                     swap(intArray, i, i + 1);
                 }
             }
