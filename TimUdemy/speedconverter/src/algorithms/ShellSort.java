6ddd16f30c2a0a06371f2378b7e93fa2bdbd080d
Shane Kaestner <shane.kaestner@yahoo.com>
2021-01-12 20:50:50 -0800

    updating shell sort algorithm

--- TimUdemy/speedconverter/src/algorithms/ShellSort.java
+++ TimUdemy/speedconverter/src/algorithms/ShellSort.java
@@ -7,7 +7,32 @@ package algorithms;
  * with each iteration until it is finally 1.
  * One very common way to calculate the gap value is called Knuth Sequence where
  * the gap value is calculated using (3^k - 1) / 2
+ * Shell sort wikipedia page for reference - https://en.wikipedia.org/wiki/Shellsort
+ * shell sort is an in place algorithm and unstable
+ * shell sort can also improve the efficiency of bubble sort
  */
 
 public class ShellSort {
+    public static void main(String[] args) {
+
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
+
+            for (int i = gap; i < intArray.length; i++) {
+                int newElement = intArray[i];
+
+                int j = i;
+
+                while (j >= gap && intArray[j - gap] > newElement) {
+                    intArray[j] = intArray[j - gap];
+                }
+            }
+
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
 }
