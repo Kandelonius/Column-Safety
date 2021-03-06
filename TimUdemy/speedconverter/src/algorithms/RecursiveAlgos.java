76afe07bd8c703a59c6fdc3e6763d05ef5390443
Shane Kaestner <shane.kaestner@yahoo.com>
2021-03-05 23:07:31 -0800

    updated a recursive algorithm.

--- TimUdemy/speedconverter/src/algorithms/RecursiveAlgos.java
+++ TimUdemy/speedconverter/src/algorithms/RecursiveAlgos.java
@@ -3,6 +3,24 @@ package algorithms;
 public class RecursiveAlgos {
     public static void main(String[] args) {
 
+        System.out.println("factorial 0 is " + recursiveFactorial(0));
+        System.out.println("factorial 5 is " + recursiveFactorial(5));
+
+    }
+        /*
+            1! -0 != 1 - 1
+            2! = 2 * 1 = 2 * 1!
+            3! = 3 * 2 * 1 = 3 * 2!
+            4! = 4 * 3 * 2 * 1 = 4 * 3!
+
+            n! = n *(n - 1)!
+         */
+
+    public static int recursiveFactorial(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        return num * recursiveFactorial(num - 1);
     }
 
     public static int iterativeFactorial(int num) {
@@ -16,7 +34,7 @@ public class RecursiveAlgos {
         }
 
         return factorial;
-        
+
     }
 
 }
