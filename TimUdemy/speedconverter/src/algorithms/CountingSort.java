31c4aa9474d4e20a6c42d1e3cc02c6e464823fda
Shane Kaestner <shane.kaestner@yahoo.com>
2021-05-21 21:10:39 -0700

    implemented counting sort and updated Main to be able to run it.

--- TimUdemy/speedconverter/src/algorithms/CountingSort.java
+++ TimUdemy/speedconverter/src/algorithms/CountingSort.java
@@ -1,7 +1,26 @@
 package algorithms;
 
-public class CountingSort {
-    public static void sort(int[] array) {
+/*
+    Makes assumptions about the data, doesn't use comparisons. Counts the number
+    of occurrences, only works with non-negative discrete values. Uses a specific
+    range.
+ */
 
+public class CountingSort {
+    public static void sort(int[] array, int min, int max) {
+
+        int[] countArray = new int[(max - min) + 1];
+
+        for (int i = 0; i < array.length; i++) {
+            countArray[array[i] - min]++;
+        }
+
+        int j = 0;
+        for (int i = min; i <= max; i++) {
+            while (countArray[i - min] > 0) {
+                array[j++] = i;
+                countArray[i - min]--;
+            }
+        }
     }
 }
--- TimUdemy/speedconverter/src/algorithms/Main.java
+++ TimUdemy/speedconverter/src/algorithms/Main.java
@@ -5,6 +5,7 @@ package algorithms;
 public class Main {
     public static void main(String[] args) {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+        int[] positiveArray = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
 
 //        BubbleSort.sort(intArray);
 //        InsertionSort.sort(intArray);
@@ -12,7 +13,9 @@ public class Main {
 //        QuickSort.sort(intArray);
 //        SelectionSort.sort(intArray);
 //        ShellSort.sort(intArray);
+        CountingSort.sort(positiveArray, 1, 10);
 
-        Helper.printSorted(intArray);
+//        Helper.printSorted(intArray); // use for sorts that allow negative
+        Helper.printSorted(positiveArray); // use for sorts that don't allow negative
     }
 }
