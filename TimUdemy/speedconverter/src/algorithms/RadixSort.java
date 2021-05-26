a55ac93cf04a808cf2c117eca1565a445dc81715
Shane Kaestner <shane.kaestner@yahoo.com>
2021-05-25 23:48:31 -0700

    update

--- TimUdemy/speedconverter/src/algorithms/RadixSort.java
+++ TimUdemy/speedconverter/src/algorithms/RadixSort.java
@@ -6,7 +6,7 @@ package algorithms;
     moves left each step because the rightmost element is often the least impactful digit. Can be O(n),
     but is often slower than O(nlogn)
 
-    *Radix is the amount of possible outcomes or digits a unit can have ie. a binary has 2 possible
+    *Radix is the amount of possible outcomes or unique digits a unit can have ie a binary has 2 possible
     states '0' or '1' so its radix is 2 whereas the english alphabet has 26 unique letters so its radix
     is 26. Keep in mind the radix for a decimal number is 10 not infinity. Width in this instance refers
     to the amount of digits or characters in the elements. 'cool' and '6174' both have a width of 4 but since
@@ -15,6 +15,6 @@ package algorithms;
 
 public class RadixSort {
     public static void sort(int[] arr) {
-        
+
     }
 }
